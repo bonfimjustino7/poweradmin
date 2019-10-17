f4d2bf81a8d49d40f26cac7cc0d8fe7f3cc9cf7c
Bonfim Justino <bonfim1999@gmail.com>
2019-10-17 09:28:47 -0300

    use poweradmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from poweradmin.admin import *
+from .models import Aluno
 
-# Register your models here.
+class MyModelAdmin(PowerModelAdmin):
+    def get_buttons(self, request, object_id):
+        buttons = super(MyModelAdmin, self).get_buttons(request, object_id)
+        if object_id: # Button into change
+            buttons.append(PowerButton(url=reverse('admin:myapp_mymodel_changelist'), label=u'Back'))
+        else: # Button on list display
+            buttons.append(PowerButton(url=u'http://google.com.br/', label=u'Google', attrs={'target': '_blank'}))
+        return buttons
+
+admin.site.register(Aluno, MyModelAdmin)
