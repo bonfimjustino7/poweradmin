c46154187ae941cebed46a03f74d931adaf6f69a
Bonfim Justino <bonfim1999@gmail.com>
2019-10-17 09:57:51 -0300

    Corrigindo reverse

--- core/admin.py
+++ core/admin.py
@@ -7,7 +7,7 @@ class MyModelAdmin(PowerModelAdmin):
     def get_buttons(self, request, object_id):
         buttons = super(MyModelAdmin, self).get_buttons(request, object_id)
         if object_id: # Button into change
-            buttons.append(PowerButton(url=reverse('admin:myapp_mymodel_changelist'), label=u'Back'))
+            buttons.append(PowerButton(url=reverse('admin:core_aluno_changelist'), label=u'Back'))
         else: # Button on list display
             buttons.append(PowerButton(url=u'http://google.com.br/', label=u'Google', attrs={'target': '_blank'}))
         return buttons
@@ -18,6 +18,6 @@ class MyModelAdmin(PowerModelAdmin):
     )
 
     list_csv = ('nome', 'idade',)
-    list_display = ('nome', 'idade', )
+    list_display = ('nome', 'idade',)
 
 admin.site.register(Aluno, MyModelAdmin)
